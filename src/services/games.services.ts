aeb0a1862e00d6c44316e8da359ac340ac4db69d
elVengador <dev.jimynicanor@gmail.com>
2025-10-06 23:27:25 -0500

    refactor: update games service url

--- src/services/games.services.ts
+++ src/services/games.services.ts
@@ -1,7 +1,7 @@
 import { GetGamesOutput } from "@/types";
 
 export const getGames = async (genre: string = '', page: number = 1): Promise<GetGamesOutput> => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_URI}/games?genre=${genre}&page=${page}`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URI}/api/games?genre=${genre}&page=${page}`);
     const data = res.json();
     return data;
 };
