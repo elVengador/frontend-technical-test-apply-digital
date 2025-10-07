fef037cc3fc52661512f2d4f000ac7cf1173f666
elVengador <dev.jimynicanor@gmail.com>
2025-10-06 23:57:23 -0500

    refactor: update uri to use nextjs env variable

--- src/services/games.services.ts
+++ src/services/games.services.ts
@@ -1,7 +1,12 @@
 import { GetGamesOutput } from "@/types";
 
+const isProd = process.env.NODE_ENV === 'production';
+export const API_URI = isProd
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
+    : 'http://localhost:3000/api';
+
 export const getGames = async (genre: string = '', page: number = 1): Promise<GetGamesOutput> => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_URI}/api/games?genre=${genre}&page=${page}`);
+    const res = await fetch(`${API_URI}/games?genre=${genre}&page=${page}`);
     const data = res.json();
     return data;
 };
